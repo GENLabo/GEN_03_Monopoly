1f7895a1a3d2437f1dd327087b5d2dd9189e4b07
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-04-09 13:50:40 +0200

    Add player random generation

--- src/main/java/MGame.java
+++ src/main/java/MGame.java
@@ -1,28 +1,45 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MGame {
 
-    final int MIN_PLAYERS_NB = 2;
-    final int MAX_PLAYERS_NB = 8;
-    final int DICE_NB = 2;
-    final int ROUNDS_NB = 20;
+    private final ArrayList<String> PLAYERS_NAMES = new ArrayList<String>(
+            Arrays.asList("Loïc", "Robin", "Vitor", "Charles", "Lucie", "Luc", "Alex", "Luna")
+    );
 
-    ArrayList<Player> players;
-    ArrayList<Die> dice;
-    Random rand = new Random();
+    private final int MIN_PLAYERS_NB = 2;
+    private final int MAX_PLAYERS_NB = 8;
+    private final int DICE_NB = 2;
+    private final int ROUNDS_NB = 20;
+
+    private ArrayList<Player> players;
+    private ArrayList<Die> dice;
+    private Random rand = new Random();
 
     /**
      * Public constructor, game initialisation
-     * Generate a random number of players between [2,8]
+     * Generate a random number of players
      * Generate 2 dice
      */
     public MGame(){
-        // Generate a random nb of players
-        int randPlayersNb = rand.nextInt((MAX_PLAYERS_NB - MIN_PLAYERS_NB) + 1) + MIN_PLAYERS_NB;
-        System.out.println(randPlayersNb);
-        players = new ArrayList<>(randPlayersNb);
+        generatePlayers();
         dice = new ArrayList<>(DICE_NB);
+    }
+
+    /**
+     * Generate a random nb of players between [2,8]
+     * with random name related to the PLAYERS_NAME ArrayList
+     */
+    private void generatePlayers(){
+        int randPlayersNb = rand.nextInt((MAX_PLAYERS_NB - MIN_PLAYERS_NB) + 1) + MIN_PLAYERS_NB;
+        players = new ArrayList<>();
+        for(int i = 0; i < randPlayersNb; ++i){
+            // Pick a random name and create player
+            int randName = rand.nextInt(MAX_PLAYERS_NB );
+            players.add(new Player(PLAYERS_NAMES.get(randName)));
+        }
     }
 
     /**
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
-        Die die = new Die();
-
-        for(int i = 0; i < 10; ++i){
-            die.roll();
-            System.out.println(die.getFaceValue());
-        }
-
+        MGame mGame = new MGame();
+        System.out.println("dee");
     }
 
 }
